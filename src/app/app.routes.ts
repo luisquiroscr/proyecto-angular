54b68e162797a7b4b24494aa078629d68d0a4313
luisquiroscr <lquiros@vlacademy.com>
2025-05-26 21:51:47 -0600

    Componente Example funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ExampleComponent } from './example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'example', component: ExampleComponent },
+];
